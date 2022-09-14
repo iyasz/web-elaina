2a32b620bc5b395913c8df90d7d23527267024bd
Yasz Avellia <zakamaragames@gmail.com>
2022-09-14 14:58:27 +0700

    menambah animasi col body

--- layouts/header.php
+++ layouts/header.php
@@ -17,7 +17,7 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-transparent animate__animated animate__fadeInDown">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="index.php?page=home">Elaina:3</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
---
+++ views/page/home.php
@@ -0,0 +1,13 @@
+<div class="row animate__animated animate__fadeInRight">
+    <div class="col ">
+        <h1 class="fw-500 text-bd ">Elaina <br> Universe Ecthical </h1>
+        <div class="mt-3 ">
+            <div class="input-group">
+                <span class="input-group-text border-0 bg-wh">
+                    <i class='bx bx-user'></i>
+                </span>
+                <input type="text" autocomplete="off" class="form-control bg-wh" name="email" placeholder="Masukan Email" required>
+            </div>
+        </div>
+    </div>
+</div>
