07264bd8fcd37c506cbb3dbf7cf20b8e4a8b6e18
Yasz Avellia <zakamaragames@gmail.com>
2022-09-14 15:00:00 +0700

    menambah input ype

--- views/page/home.php
+++ views/page/home.php
@@ -1,10 +1,10 @@
 <div class="row animate__animated animate__fadeInRight">
-    <div class="col ">
+    <div class="col-lg-6">
         <h1 class="fw-500 text-bd ">Elaina <br> Universe Ecthical </h1>
         <div class="mt-3 ">
             <div class="input-group">
                 <span class="input-group-text border-0 bg-wh">
-                    <i class='bx bx-user'></i>
+                    <i class='bx bx-search'></i>
                 </span>
                 <input type="text" autocomplete="off" class="form-control bg-wh" name="email" placeholder="Masukan Email" required>
             </div>
