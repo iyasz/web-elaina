d2c197e47313c86a0a260434f768612b718c7470
Yasz Avellia <zakamaragames@gmail.com>
2022-09-15 19:31:40 +0700

    mencoba teks hero

--- views/page/home.php
+++ views/page/home.php
@@ -1,13 +1,25 @@
-<div class="row animate__animated animate__fadeInRight">
-    <div class="col-lg-6">
+<!-- <div class="row ">
+    <div class="col-lg-6 animate__animated animate__fadeInRight">
         <h1 class="fw-500 text-bd ">Elaina <br> Universe Ecthical </h1>
-        <div class="mt-3 ">
-            <div class="input-group">
-                <span class="input-group-text border-0 bg-wh">
-                    <i class='bx bx-search'></i>
-                </span>
-                <input type="text" autocomplete="off" class="form-control bg-wh" name="email" placeholder="Masukan Email" required>
+        <dv class="row justify-content-center">
+            <div class="col-lg-5">
+                <div class="mt-3 ">
+                    <div class="input-group">
+                        <span class="input-group-text border-0 bg-wh">
+                            <i class='bx bx-search'></i>
+                        </span>
+                        <input type="text" autocomplete="off" class="form-control bg-wh" name="email" placeholder="Masukan Email" required>
+                    </div>
+                </div>
             </div>
+        </dv>
+    </div>
+</div> -->
+<div class="container ">
+    <div class="row align-items-start">
+        <div class="col-sm-7">
+            <h1 class="text-bd">Elaina <br> Universe Ecthical</h1>
         </div>
+
     </div>
 </div>
